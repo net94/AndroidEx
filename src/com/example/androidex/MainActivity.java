f9b564e773e6226cdd9b1edb93e125a9730bb5eb
mskim <net94@hanmail.net>
2012-10-24 15:10:04 +0900

    layout 수정

--- src/com/example/androidex/MainActivity.java
+++ src/com/example/androidex/MainActivity.java
@@ -10,6 +10,8 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        
     }
 
     @Override
